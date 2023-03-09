7263f637fe800ac03861d29f53ae1dc5de292992
HasanMohammadSadaqa <hasansadaqa55@gmail.com>
2023-03-09 20:57:17 +0200

    update

--- Basics/Advance Strings/is_palindrome.py
+++ Basics/Advance Strings/is_palindrome.py
@@ -1,6 +1,6 @@
 string = input("please enter a string to check: ")
-while string == string[::-1]:
+if string == string[::-1]:
     print("the string is palindrome")
-    break
+
 else:
     print("the string is not palindrome")
