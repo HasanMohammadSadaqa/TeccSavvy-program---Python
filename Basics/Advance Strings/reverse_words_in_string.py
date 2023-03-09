f5c92ab76c9b22e160833251d9ff63bc175d7e33
HasanMohammadSadaqa <hasansadaqa55@gmail.com>
2023-03-09 21:19:17 +0200

    updates

--- Basics/Advance Strings/reverse_words_in_string.py
+++ Basics/Advance Strings/reverse_words_in_string.py
@@ -1,7 +1,8 @@
-str = input("please enter a string to reverse the words: ").split()
+str = input("please enter a string to reverse the words: ")
 if " " in str:
-    str = str[::-1]
-    str = " ".join(str)
-    print(f"the reversed string is: {str}")
+    new_str = str.split()
+    new_str = new_str[::-1]
+    # new_str = " ".join(str)
+    print(f"the reversed string is: {new_str}")
 else:
-    print(f"the string is {str[0]} \n note that the string must have more than word to be reversed ")
+    print("note that the string must have more than word to be reversed ")
