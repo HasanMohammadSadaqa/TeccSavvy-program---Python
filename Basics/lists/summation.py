ea8e0293b6807a6e607e3867e5acb761f4328e0b
HasanMohammadSadaqa <hasansadaqa55@gmail.com>
2023-03-13 00:23:41 +0200

    summation of all number in a list

---
+++ Basics/lists/summation.py
@@ -0,0 +1,9 @@
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+summation = 0
+for num in list:
+    summation += num
+print(f"the summation of {list} is {summation}")
+
+# second method
+Sum = sum(list)
+print(f"the summation of {list} is {Sum}")
