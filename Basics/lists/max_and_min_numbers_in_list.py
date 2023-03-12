9e5face73e49abb16669f86589658dee97130424
HasanMohammadSadaqa <hasansadaqa55@gmail.com>
2023-03-12 23:27:46 +0200

    finding min and max number of a list

---
+++ Basics/lists/max_and_min_numbers_in_list.py
@@ -0,0 +1,14 @@
+list = [1, 5, 6, 115, 30, -110]
+max_num = max(list)
+min_num = min(list)
+print(f"{list}\nmaximum number is: {max_num}\nminimum number is: {min_num}")
+
+# second method
+# max_number = list[0]
+# min_number = list[-1]
+# for num in list:
+#     if num > max_number:
+#         max_number = num
+#     if num < min_number:
+#         min_number = num
+# print(f"{list}\nmaximum number is: {max_num}\nminimum number is: {min_num}")
