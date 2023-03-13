6a8c2216add3d5a5a582895d243f05a172f573e7
HasanMohammadSadaqa <hasansadaqa55@gmail.com>
2023-03-13 20:26:08 +0200

    function takes a string and return number of vowels in that string

---
+++ Basics/Functions/number_of_vowels.py
@@ -0,0 +1,10 @@
+def number_of_vowels(string):
+    vowels = "aeiouAEIOU"
+    count = 0
+    for char in string:
+        if char in vowels:
+            count += 1
+    return count
+
+
+print(number_of_vowels("Hasan"))
