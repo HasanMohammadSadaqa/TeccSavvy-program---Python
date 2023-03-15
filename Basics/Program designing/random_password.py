1c270ec869db3a62bdccf048f7986eb1d8b677ca
HasanMohammadSadaqa <hasansadaqa55@gmail.com>
2023-03-15 15:58:10 +0200

    some updates

--- Basics/Program designing/random_password.py
+++ Basics/Program designing/random_password.py
@@ -3,9 +3,10 @@ this program prompt the user to enter a specific length for a password and print
 """
 import random
 import string
+import sys
 
 
-def random_password(length):
+def string_random_password(length):
     """
 
     :param length: length of a password
@@ -13,10 +14,26 @@ def random_password(length):
     """
     return random.choices(string.ascii_letters, k=length)
 
-
+def integer_random_password(length):
+    range_start = 10**(length-1)
+    end_range = (10**length)-1
+    return random.randint(range_start, end_range)
 while True:
     try:
-        length = int(input("enter the length you want: "))
-        print("".join(random_password(length)))
+        type_of_password = input("what you type of password you want(string/integer)? ")
+        if type_of_password.lower() == "string":
+            try:
+                length = int(input("enter the length you want: "))
+                print("".join(string_random_password(length)))
+                break
+            except ValueError:
+                print("Invalid input, please try again")
+        elif type_of_password.lower() == "integer":
+            length = int(input("enter the length you want: "))
+            print(integer_random_password(length))
+            break
+        else:
+            print("please write string or integer")
     except ValueError:
-        print("Invalid input, please try again")
+        sys.exit("thanks for using the program")
+
