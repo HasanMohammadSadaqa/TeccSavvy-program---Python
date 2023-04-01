41ad768e76801f308422e4c00b5831b2d6334707
HasanMohammadSadaqa <hasansadaqa55@gmail.com>
2023-04-01 21:37:12 +0300

    some update when the list has an element with diifrent types

--- Basics/lists/max_and_min_numbers_in_list.py
+++ Basics/lists/max_and_min_numbers_in_list.py
@@ -12,3 +12,13 @@ print(f"{list}\nmaximum number is: {max_num}\nminimum number is: {min_num}")
 #     if num < min_number:
 #         min_number = num
 # print(f"{list}\nmaximum number is: {max_num}\nminimum number is: {min_num}")
+
+# third method if the list has element not numbers
+# list1 = [True, "Hasan", 1,2,3,4,5,6,7,8,9]
+# new_list = []
+# for item in list1:
+#     if type(item) == int:
+#         new_list.append(item)
+# max_number = max(new_list)
+# min_number = min(new_list)
+# print(max_number, min_number)
