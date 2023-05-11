efa746519b8541cdfac633aa066afe61afa385ca
HasanMohammadSadaqa <hasansadaqa55@gmail.com>
2023-05-11 20:40:58 +0300

    updates

--- Intermeiate/Advance Data Type/Functional programmig/Warm Up Exercises/recursion.py
+++ Intermeiate/Advance Data Type/Functional programmig/Warm Up Exercises/recursion.py
@@ -1,10 +1,12 @@
 def factorial(num):
+    if num < 0:
+        return "number must be greater than zero "
     if num == 0 or num == 1:
         return 1
     else:
         return num * factorial(num - 1)
 
 
-number = 5
+number = -1
 result = factorial(number)
 print(f"The factorial of {number} is: {result}")
